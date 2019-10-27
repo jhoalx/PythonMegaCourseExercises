0a86f694e2b4f3482eeb48a9bce952644082d21a
John Alejandro Zapata <jhoalx@gmail.com>
2019-10-27 17:45:57 -0500

    Exercise 18 - Access and Append

--- 04-the_basics-operations_with_data_types/main.py
+++ 04-the_basics-operations_with_data_types/main.py
@@ -63,3 +63,10 @@ print(serials[2])
 # Access 1st, 3rd, 6th items
 serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
 print(serials[0], serials[2], serials[5])
+
+################################################################################
+# append 1st weekend to workdays (oh, please, no)
+workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+weekend = ["Sat", "Sun"]
+workdays.append(weekend[0])
+print(workdays)
