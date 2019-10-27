2e20ab55f1b857ea5c9018c0ad52edb32e12d005
John Alejandro Zapata <jhoalx@gmail.com>
2019-10-27 11:20:56 -0500

    Lecture 13 - Ranges

--- 03-the_basics-data_types/main.py
+++ 03-the_basics-data_types/main.py
@@ -11,9 +11,10 @@ myText = "Leet"
 print(myNumber, myText)
 
 ################################################################################
-x = 10
-y = '10'
-z = 13.37
+# Simple Types
+x = 10  # integer
+y = '10'  # string
+z = 13.37  # float
 
 sum1 = x + x
 sum2 = y + y
@@ -22,6 +23,13 @@ print(sum1, sum2)
 print(type(x), type(y), type(z))
 
 ################################################################################
+# List Types
 student_grades = [9.1, 8.8, 7.5]
 
 ################################################################################
+# Ranges
+rangeList = list(range(1, 10))
+print(rangeList)
+
+rangeListWithStep = list(range(1, 10, 2))
+print(rangeListWithStep)
