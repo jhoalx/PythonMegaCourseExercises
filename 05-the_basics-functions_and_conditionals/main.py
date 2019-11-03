58a45668521f34584d8f01fde7dc1cf0a6020492
John Alejandro Zapata <jhoalx@gmail.com>
2019-11-03 13:05:45 -0500

    Lecture 32 - Creating Your Own Functions

---
+++ 05-the_basics-functions_and_conditionals/main.py
@@ -0,0 +1,10 @@
+
+
+# define a function (pro style, with type-hints and return type)
+# example purpose only, there's already a mean function in 'statistics' module
+def mean(number_list: list) -> int:
+    result: int = sum(number_list) / len(number_list)
+    return result
+
+
+print(mean([1, 3, 5, 10]))  # Should output 4.75
