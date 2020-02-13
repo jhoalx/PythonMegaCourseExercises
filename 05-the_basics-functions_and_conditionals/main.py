d59d43135e1e206373c95ab684edfc24ace1cbd5
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-13 16:04:26 -0500

    Exercise 23 - Square area

--- 05-the_basics-functions_and_conditionals/main.py
+++ 05-the_basics-functions_and_conditionals/main.py
@@ -1,5 +1,3 @@
-
-
 # define a function (pro style, with type-hints and return type)
 # example purpose only, there's already a mean function in 'statistics' module
 def mean(number_list: list) -> int:
@@ -8,3 +6,13 @@ def mean(number_list: list) -> int:
 
 
 print(mean([1, 3, 5, 10]))  # Should output 4.75
+
+
+################################################################################
+
+# Define a function that calculates the area of a square.
+def square_area(side: int) -> int:
+    return side * side
+
+
+print("square area for side length 5: ", square_area(5))
