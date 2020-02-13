17afef09430779b989f2dfa16e076559e652bb97
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-13 16:34:56 -0500

    Exercise 24 - Volume converter

--- 05-the_basics-functions_and_conditionals/main.py
+++ 05-the_basics-functions_and_conditionals/main.py
@@ -1,5 +1,7 @@
 # define a function (pro style, with type-hints and return type)
 # example purpose only, there's already a mean function in 'statistics' module
+
+
 def mean(number_list: list) -> int:
     result: int = sum(number_list) / len(number_list)
     return result
@@ -16,3 +18,17 @@ def square_area(side: int) -> int:
 
 
 print("square area for side length 5: ", square_area(5))
+
+
+################################################################################
+
+# Define a function that converts fluid ounces to milliliters knowing that
+# 1 fluid ounce is equal to 29.57353 milliliters. For example, I was to call
+# your function with foo(1)  I would get an output of 29.57353
+# If I called it with foo(5)  I would get 147.86765, and so on
+
+def ounces_to_milliliters(ounces: float) -> float:
+    return ounces * 29.57353
+
+
+print("5 ounces to milliliters: ", ounces_to_milliliters(5))
