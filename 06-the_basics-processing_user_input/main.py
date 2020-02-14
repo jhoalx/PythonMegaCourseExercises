b4f146798a645cda7efc2993b413c15396c2cb03
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-14 11:58:20 -0500

    Lecture 43 - String Formatting

--- 06-the_basics-processing_user_input/main.py
+++ 06-the_basics-processing_user_input/main.py
@@ -5,5 +5,23 @@ def weather_condition(temperature: float) -> str:
         return "Cold"
 
 
-current_temperature = float(input("Please type current temperature"))
+current_temperature = float(input("Please type current temperature: "))
 print(weather_condition(current_temperature))
+
+################################################################################
+
+# String formatting
+user_input = input("Please, enter your name: ")
+
+# %-formatting
+# Unfortunately, this kind of formatting isn’t great because it is verbose and
+# leads to errors, like not displaying tuples or dictionaries correctly
+message1 = "Hello %s, nice to meet you" % user_input
+
+# recommended formatting method, Literal String Interpolation
+# or more commonly called "F-strings"
+# F-strings are fast! Much faster than %-formatting and str.format()
+# notice the F before the double-quotes
+message2 = F"Another greeting {user_input}, have a nice day"
+print(message1)
+print(message2)
