58014ee0a0dd60390c9db8552a4bad4b08677068
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-14 08:32:46 -0500

    Lecture 36 - Bonus Code: Using "and" and "or" in a Conditional

--- 05-the_basics-functions_and_conditionals/main.py
+++ 05-the_basics-functions_and_conditionals/main.py
@@ -52,3 +52,40 @@ def mean(value: list or dict) -> float:
 
 student_grades = {"ruben": 9.1, "joan": 9.2, "mauro": 9.3}
 print("student grades mean: ", mean(student_grades))
+
+################################################################################
+
+# Bonus Code: Using "and" and "or" in a Conditional
+# You learned to check for one single condition:
+
+x = 1
+
+if x == 1:
+    print("Yes")
+else:
+    print("No")
+
+# You can also check if two conditions are met
+# at the same time using an and operator:
+
+x = 1
+y = 1
+
+if x == 1 and y == 1:
+    print("Yes")
+else:
+    print("No")
+# That will return Yes since x == 1 and y ==1 are both True.
+
+
+# You can also check if one of two conditions are met using an or operator
+
+x = 1
+y = 1
+
+if x == 1 or y == 2:
+    print("Yes")
+else:
+    print("No")
+
+# That will return Yes since at least one of the conditions is True. In this case x == 1 is True.
