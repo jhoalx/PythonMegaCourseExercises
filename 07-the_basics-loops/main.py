48a1ac6a214a492d6f6dd57bb2bb240e12dae110
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-14 15:49:35 -0500

    Exercise 30 - Loop over colors

--- 07-the_basics-loops/main.py
+++ 07-the_basics-loops/main.py
@@ -4,7 +4,14 @@ friday_temperatures = [9.1, 8.8, 7.6]
 for temperature in friday_temperatures:
     print(round(temperature))
 
-
 # loop trough string characters
 for letter in "hello":
     print(letter)
+
+################################################################################
+# Exercise 30 - Loop over colors
+
+colors = [11, 34, 98, 43, 45, 54, 54]
+
+for color in colors:
+    print(color)
