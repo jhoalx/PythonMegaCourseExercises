5d5e25458ca33210628aa7ca6f6776fd8687ba19
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-14 15:41:32 -0500

    Lecture 46 - For loops: How and Why

---
+++ 07-the_basics-loops/main.py
@@ -0,0 +1,10 @@
+# FOR loops
+
+friday_temperatures = [9.1, 8.8, 7.6]
+for temperature in friday_temperatures:
+    print(round(temperature))
+
+
+# loop trough string characters
+for letter in "hello":
+    print(letter)
