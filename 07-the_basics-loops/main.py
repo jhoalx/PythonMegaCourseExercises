1804a1ecef66cb7c91f535226697d0e308f267ce
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-14 15:52:59 -0500

    Exercise 31 - Loop over big colors

--- 07-the_basics-loops/main.py
+++ 07-the_basics-loops/main.py
@@ -15,3 +15,14 @@ colors = [11, 34, 98, 43, 45, 54, 54]
 
 for color in colors:
     print(color)
+
+
+################################################################################
+# Exercise 31 - Loop over big colors
+# Loop over the color items and print out the item in every loop only if the
+# item is greater than 50
+colors = [11, 34, 98, 43, 45, 54, 54]
+
+for color in colors:
+    if color > 50:
+        print(color)
