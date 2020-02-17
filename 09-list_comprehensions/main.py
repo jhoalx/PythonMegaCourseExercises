4ab72a2f592828fb9dec6253a73b1cc120215f62
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-17 16:41:21 -0500

    Exercise 39 - Convert and Sum Up

--- 09-list_comprehensions/main.py
+++ 09-list_comprehensions/main.py
@@ -91,3 +91,21 @@ def foo3(input_list: list) -> list:
 
 
 print(foo3([99, 'no data', 95, 94, 'no data']))
+
+
+################################################################################
+
+# Exercise 39 - Convert and Sum Up
+
+# Define a function that takes as parameter a list that contains decimal numbers
+# as strings and returns the sum of those numbers.
+# For example, I called your function with
+# foo(['1.2', '2.6', '3.3'])
+# it should return 7.1
+# Note that the floats of the input list are string datatypes.
+
+def foo4(input_list: list) -> float:
+    return sum([float(val) for val in input_list])
+
+
+print(foo4(['1.2', '2.6', '3.3']))
