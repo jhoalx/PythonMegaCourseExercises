4dc12da0135f9d2191bbf262d98b43e4055a4583
John Alejandro Zapata <jhoalx@gmail.com>
2020-02-18 09:49:23 -0500

    # Lecture 71 - Reading Text From a File

---
+++ 11-file_processing/main.py
@@ -0,0 +1,12 @@
+# Lecture 71 - Reading Text From a File
+
+my_file = open("fruits.txt")
+print(type(my_file))  # <class '_io.TextIOWrapper'>
+
+# The file object returned by open() function is an
+# object of type _io.TextIOWrapper.
+# The class _io.TextIOWrapper provides methods and attributes which helps us
+# to read or write data to and from the file. The following table lists some
+# commonly used methods of _io.TextIOWrapper class.
+
+print(my_file.read())
